0d1c8c1d857787eeb786e4bfe351f9767d15d5d4
Dan Chen <dan.chen@boardware.com>
2023-08-09 10:46:03 +0800

    fixup

--- coreapi.go
+++ coreapi.go
@@ -6,6 +6,79 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type AccountList struct {
+	Pagination Pagination `json:"pagination" binding:"required"`
+	Data       []Account  `json:"data" binding:"required"`
+}
+type Restaurant struct {
+	Description string      `json:"description" binding:"required"`
+	Itmes       *[]ItemList `json:"itmes,omitempty"`
+	Id          string      `json:"id" binding:"required"`
+	Name        string      `json:"name" binding:"required"`
+}
+type RestaurantList struct {
+	Pagination Pagination   `json:"pagination" binding:"required"`
+	Data       []Restaurant `json:"data" binding:"required"`
+}
+type Table struct {
+	Id    string `json:"id" binding:"required"`
+	Label string `json:"label" binding:"required"`
+}
+type Option struct {
+	Label string `json:"label" binding:"required"`
+	Extra int64  `json:"extra" binding:"required"`
+}
+type SessionVerificationRequest struct {
+	Token string `json:"token" binding:"required"`
+}
+type CreateSessionRequest struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+type Account struct {
+	Id    string `json:"id" binding:"required"`
+	Email string `json:"email" binding:"required"`
+	Role  Role   `json:"role" binding:"required"`
+}
+type SessionVerification struct {
+	Status SessionStatus `json:"status" binding:"required"`
+}
+type Pagination struct {
+	Index int64 `json:"index" binding:"required"`
+	Limit int64 `json:"limit" binding:"required"`
+	Total int64 `json:"total" binding:"required"`
+}
+type TableList struct {
+	Data *[]Table `json:"data,omitempty"`
+}
+type PutTableRequest struct {
+	Label string `json:"label" binding:"required"`
+}
+type OrderItem struct {
+	Item    Item      `json:"item" binding:"required"`
+	Options *[]Option `json:"options,omitempty"`
+}
+type Bill struct {
+	CheckoutUrl string      `json:"checkoutUrl" binding:"required"`
+	Items       []OrderItem `json:"items" binding:"required"`
+}
+type PutItemRequest struct {
+	Name       string      `json:"name" binding:"required"`
+	Pricing    int64       `json:"pricing" binding:"required"`
+	Attributes []Attribute `json:"attributes" binding:"required"`
+	Images     []string    `json:"images" binding:"required"`
+}
+type PutRestaurantRequest struct {
+	Name        string  `json:"name" binding:"required"`
+	Description *string `json:"description,omitempty"`
+}
+type Uploading struct {
+	Url string `json:"url" binding:"required"`
+}
+type ItemList struct {
+	Data       []Item     `json:"data" binding:"required"`
+	Pagination Pagination `json:"pagination" binding:"required"`
+}
 type CreateAccountRequest struct {
 	VerificationCode *string `json:"verificationCode,omitempty"`
 	Email            string  `json:"email" binding:"required"`
@@ -16,65 +89,6 @@ type UpdatePasswordRequest struct {
 	Password    string `json:"password" binding:"required"`
 	NewPassword string `json:"newPassword" binding:"required"`
 }
-type Account struct {
-	Id    string `json:"id" binding:"required"`
-	Email string `json:"email" binding:"required"`
-	Role  Role   `json:"role" binding:"required"`
-}
-type OrderItem struct {
-	Item    Item      `json:"item" binding:"required"`
-	Options *[]Option `json:"options,omitempty"`
-}
-type Uploading struct {
-	Url string `json:"url" binding:"required"`
-}
-type CreateSessionRequest struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
-}
-type ItemList struct {
-	Data       []Item     `json:"data" binding:"required"`
-	Pagination Pagination `json:"pagination" binding:"required"`
-}
-type SessionVerificationRequest struct {
-	Token string `json:"token" binding:"required"`
-}
-type AccountList struct {
-	Pagination Pagination `json:"pagination" binding:"required"`
-	Data       []Account  `json:"data" binding:"required"`
-}
-type SessionVerification struct {
-	Status SessionStatus `json:"status" binding:"required"`
-}
-type RestaurantList struct {
-	Data       []Restaurant `json:"data" binding:"required"`
-	Pagination Pagination   `json:"pagination" binding:"required"`
-}
-type PutTableRequest struct {
-	Label string `json:"label" binding:"required"`
-}
-type Bill struct {
-	CheckoutUrl string      `json:"checkoutUrl" binding:"required"`
-	Items       []OrderItem `json:"items" binding:"required"`
-}
-type Pagination struct {
-	Index int64 `json:"index" binding:"required"`
-	Limit int64 `json:"limit" binding:"required"`
-	Total int64 `json:"total" binding:"required"`
-}
-type PutItemRequest struct {
-	Name       string      `json:"name" binding:"required"`
-	Pricing    int64       `json:"pricing" binding:"required"`
-	Attributes []Attribute `json:"attributes" binding:"required"`
-}
-type Attribute struct {
-	Label   string   `json:"label" binding:"required"`
-	Options []Option `json:"options" binding:"required"`
-}
-type Option struct {
-	Extra int64  `json:"extra" binding:"required"`
-	Label string `json:"label" binding:"required"`
-}
 type Session struct {
 	ExpiredAt   int64   `json:"expiredAt" binding:"required"`
 	CreatedAt   int64   `json:"createdAt" binding:"required"`
@@ -83,23 +97,6 @@ type Session struct {
 	TokenType   string  `json:"tokenType" binding:"required"`
 	TokenFormat string  `json:"tokenFormat" binding:"required"`
 }
-type Restaurant struct {
-	Id          string      `json:"id" binding:"required"`
-	Name        string      `json:"name" binding:"required"`
-	Description string      `json:"description" binding:"required"`
-	Itmes       *[]ItemList `json:"itmes,omitempty"`
-}
-type Table struct {
-	Id    string `json:"id" binding:"required"`
-	Label string `json:"label" binding:"required"`
-}
-type TableList struct {
-	Data *[]Table `json:"data,omitempty"`
-}
-type PutRestaurantRequest struct {
-	Name        string  `json:"name" binding:"required"`
-	Description *string `json:"description,omitempty"`
-}
 type Item struct {
 	Id         string      `json:"id" binding:"required"`
 	Name       string      `json:"name" binding:"required"`
@@ -107,6 +104,16 @@ type Item struct {
 	Attributes []Attribute `json:"attributes" binding:"required"`
 	Images     *[]string   `json:"images,omitempty"`
 }
+type Attribute struct {
+	Label   string   `json:"label" binding:"required"`
+	Options []Option `json:"options" binding:"required"`
+}
+type SessionStatus string
+
+const ACTIVED SessionStatus = "ACTIVED"
+const EXPIRED SessionStatus = "EXPIRED"
+const DISACTIVED SessionStatus = "DISACTIVED"
+
 type Ordering string
 
 const ASCENDING Ordering = "ASCENDING"
@@ -118,44 +125,18 @@ const ROOT Role = "ROOT"
 const ADMIN Role = "ADMIN"
 const USER Role = "USER"
 
-type SessionStatus string
-
-const ACTIVED SessionStatus = "ACTIVED"
-const EXPIRED SessionStatus = "EXPIRED"
-const DISACTIVED SessionStatus = "DISACTIVED"
-
 type AccountApiInterface interface {
-	VerifySession(gin_context *gin.Context, gin_body SessionVerificationRequest)
 	GetAccount(gin_context *gin.Context)
-	UpdatePassword(gin_context *gin.Context, gin_body UpdatePasswordRequest)
 	CreateAccount(gin_context *gin.Context, gin_body CreateAccountRequest)
 	ListAccount(gin_context *gin.Context, ordering Ordering, index int64, limit int64)
+	VerifySession(gin_context *gin.Context, gin_body SessionVerificationRequest)
 	CreateSession(gin_context *gin.Context, gin_body CreateSessionRequest)
+	UpdatePassword(gin_context *gin.Context, gin_body UpdatePasswordRequest)
 }
 
-func VerifySessionBuilder(api AccountApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		var sessionVerificationRequest SessionVerificationRequest
-		if err := gin_context.ShouldBindJSON(&sessionVerificationRequest); err != nil {
-			gin_context.JSON(400, gin.H{})
-			return
-		}
-		api.VerifySession(gin_context, sessionVerificationRequest)
-	}
-}
 func GetAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
 		api.GetAccount(gin_context)
-	}
-}
-func UpdatePasswordBuilder(api AccountApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		var updatePasswordRequest UpdatePasswordRequest
-		if err := gin_context.ShouldBindJSON(&updatePasswordRequest); err != nil {
-			gin_context.JSON(400, gin.H{})
-			return
-		}
-		api.UpdatePassword(gin_context, updatePasswordRequest)
 	}
 }
 func CreateAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
@@ -176,6 +157,16 @@ func ListAccountBuilder(api AccountApiInterface) func(c *gin.Context) {
 		api.ListAccount(gin_context, Ordering(ordering), stringToInt64(index), stringToInt64(limit))
 	}
 }
+func VerifySessionBuilder(api AccountApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		var sessionVerificationRequest SessionVerificationRequest
+		if err := gin_context.ShouldBindJSON(&sessionVerificationRequest); err != nil {
+			gin_context.JSON(400, gin.H{})
+			return
+		}
+		api.VerifySession(gin_context, sessionVerificationRequest)
+	}
+}
 func CreateSessionBuilder(api AccountApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
 		var createSessionRequest CreateSessionRequest
@@ -186,33 +177,129 @@ func CreateSessionBuilder(api AccountApiInterface) func(c *gin.Context) {
 		api.CreateSession(gin_context, createSessionRequest)
 	}
 }
+func UpdatePasswordBuilder(api AccountApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		var updatePasswordRequest UpdatePasswordRequest
+		if err := gin_context.ShouldBindJSON(&updatePasswordRequest); err != nil {
+			gin_context.JSON(400, gin.H{})
+			return
+		}
+		api.UpdatePassword(gin_context, updatePasswordRequest)
+	}
+}
 func AccountApiInterfaceMounter(gin_router *gin.Engine, gwg_api_label AccountApiInterface) {
-	gin_router.GET("/accounts/session/verification", VerifySessionBuilder(gwg_api_label))
 	gin_router.GET("/account", GetAccountBuilder(gwg_api_label))
-	gin_router.PUT("/account/password", UpdatePasswordBuilder(gwg_api_label))
 	gin_router.POST("/accounts", CreateAccountBuilder(gwg_api_label))
 	gin_router.GET("/accounts", ListAccountBuilder(gwg_api_label))
+	gin_router.GET("/accounts/session/verification", VerifySessionBuilder(gwg_api_label))
 	gin_router.POST("/account/session", CreateSessionBuilder(gwg_api_label))
+	gin_router.PUT("/account/password", UpdatePasswordBuilder(gwg_api_label))
 }
 
 type RestaurantApiInterface interface {
-	CreateRestaurant(gin_context *gin.Context, gin_body PutRestaurantRequest)
-	ListRestaurants(gin_context *gin.Context)
-	CreateItem(gin_context *gin.Context, id string, gin_body PutItemRequest)
-	ListRestaurantItems(gin_context *gin.Context, id string)
-	DeleteTable(gin_context *gin.Context, id string)
+	CreateTable(gin_context *gin.Context, id string, gin_body PutTableRequest)
+	ListRestaurantTable(gin_context *gin.Context, id string)
 	UpdateTable(gin_context *gin.Context, id string, gin_body PutTableRequest)
-	DeleteItem(gin_context *gin.Context, id string)
+	DeleteTable(gin_context *gin.Context, id string)
 	GetItem(gin_context *gin.Context, id string)
 	UpdateItem(gin_context *gin.Context, id string, gin_body PutItemRequest)
+	DeleteItem(gin_context *gin.Context, id string)
 	UploadItemImage(gin_context *gin.Context, id string)
 	UpdateRestaurant(gin_context *gin.Context, id string, gin_body PutRestaurantRequest)
 	GetRestaurant(gin_context *gin.Context, id string)
 	DeleteRestaurant(gin_context *gin.Context, id string)
-	CreateTable(gin_context *gin.Context, id string, gin_body PutTableRequest)
-	ListRestaurantTable(gin_context *gin.Context, id string)
+	CreateRestaurant(gin_context *gin.Context, gin_body PutRestaurantRequest)
+	ListRestaurants(gin_context *gin.Context)
+	CreateItem(gin_context *gin.Context, id string, gin_body PutItemRequest)
+	ListRestaurantItems(gin_context *gin.Context, id string)
 }
 
+func CreateTableBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		var putTableRequest PutTableRequest
+		if err := gin_context.ShouldBindJSON(&putTableRequest); err != nil {
+			gin_context.JSON(400, gin.H{})
+			return
+		}
+		api.CreateTable(gin_context, id, putTableRequest)
+	}
+}
+func ListRestaurantTableBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.ListRestaurantTable(gin_context, id)
+	}
+}
+func UpdateTableBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		var putTableRequest PutTableRequest
+		if err := gin_context.ShouldBindJSON(&putTableRequest); err != nil {
+			gin_context.JSON(400, gin.H{})
+			return
+		}
+		api.UpdateTable(gin_context, id, putTableRequest)
+	}
+}
+func DeleteTableBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.DeleteTable(gin_context, id)
+	}
+}
+func GetItemBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.GetItem(gin_context, id)
+	}
+}
+func UpdateItemBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		var putItemRequest PutItemRequest
+		if err := gin_context.ShouldBindJSON(&putItemRequest); err != nil {
+			gin_context.JSON(400, gin.H{})
+			return
+		}
+		api.UpdateItem(gin_context, id, putItemRequest)
+	}
+}
+func DeleteItemBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.DeleteItem(gin_context, id)
+	}
+}
+func UploadItemImageBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.UploadItemImage(gin_context, id)
+	}
+}
+func UpdateRestaurantBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		var putRestaurantRequest PutRestaurantRequest
+		if err := gin_context.ShouldBindJSON(&putRestaurantRequest); err != nil {
+			gin_context.JSON(400, gin.H{})
+			return
+		}
+		api.UpdateRestaurant(gin_context, id, putRestaurantRequest)
+	}
+}
+func GetRestaurantBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.GetRestaurant(gin_context, id)
+	}
+}
+func DeleteRestaurantBuilder(api RestaurantApiInterface) func(c *gin.Context) {
+	return func(gin_context *gin.Context) {
+		id := gin_context.Param("id")
+		api.DeleteRestaurant(gin_context, id)
+	}
+}
 func CreateRestaurantBuilder(api RestaurantApiInterface) func(c *gin.Context) {
 	return func(gin_context *gin.Context) {
 		var putRestaurantRequest PutRestaurantRequest
@@ -245,108 +332,22 @@ func ListRestaurantItemsBuilder(api RestaurantApiInterface) func(c *gin.Context)
 		api.ListRestaurantItems(gin_context, id)
 	}
 }
-func DeleteTableBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.DeleteTable(gin_context, id)
-	}
-}
-func UpdateTableBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		var putTableRequest PutTableRequest
-		if err := gin_context.ShouldBindJSON(&putTableRequest); err != nil {
-			gin_context.JSON(400, gin.H{})
-			return
-		}
-		api.UpdateTable(gin_context, id, putTableRequest)
-	}
-}
-func DeleteItemBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.DeleteItem(gin_context, id)
-	}
-}
-func GetItemBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.GetItem(gin_context, id)
-	}
-}
-func UpdateItemBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		var putItemRequest PutItemRequest
-		if err := gin_context.ShouldBindJSON(&putItemRequest); err != nil {
-			gin_context.JSON(400, gin.H{})
-			return
-		}
-		api.UpdateItem(gin_context, id, putItemRequest)
-	}
-}
-func UploadItemImageBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.UploadItemImage(gin_context, id)
-	}
-}
-func UpdateRestaurantBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		var putRestaurantRequest PutRestaurantRequest
-		if err := gin_context.ShouldBindJSON(&putRestaurantRequest); err != nil {
-			gin_context.JSON(400, gin.H{})
-			return
-		}
-		api.UpdateRestaurant(gin_context, id, putRestaurantRequest)
-	}
-}
-func GetRestaurantBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.GetRestaurant(gin_context, id)
-	}
-}
-func DeleteRestaurantBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.DeleteRestaurant(gin_context, id)
-	}
-}
-func CreateTableBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		var putTableRequest PutTableRequest
-		if err := gin_context.ShouldBindJSON(&putTableRequest); err != nil {
-			gin_context.JSON(400, gin.H{})
-			return
-		}
-		api.CreateTable(gin_context, id, putTableRequest)
-	}
-}
-func ListRestaurantTableBuilder(api RestaurantApiInterface) func(c *gin.Context) {
-	return func(gin_context *gin.Context) {
-		id := gin_context.Param("id")
-		api.ListRestaurantTable(gin_context, id)
-	}
-}
 func RestaurantApiInterfaceMounter(gin_router *gin.Engine, gwg_api_label RestaurantApiInterface) {
-	gin_router.POST("/restaurants", CreateRestaurantBuilder(gwg_api_label))
-	gin_router.GET("/restaurants", ListRestaurantsBuilder(gwg_api_label))
-	gin_router.POST("/restaurants/:id/items", CreateItemBuilder(gwg_api_label))
-	gin_router.GET("/restaurants/:id/items", ListRestaurantItemsBuilder(gwg_api_label))
-	gin_router.DELETE("/tables/:id", DeleteTableBuilder(gwg_api_label))
+	gin_router.POST("/restaurants/:id/tables", CreateTableBuilder(gwg_api_label))
+	gin_router.GET("/restaurants/:id/tables", ListRestaurantTableBuilder(gwg_api_label))
 	gin_router.PUT("/tables/:id", UpdateTableBuilder(gwg_api_label))
-	gin_router.DELETE("/items/:id", DeleteItemBuilder(gwg_api_label))
+	gin_router.DELETE("/tables/:id", DeleteTableBuilder(gwg_api_label))
 	gin_router.GET("/items/:id", GetItemBuilder(gwg_api_label))
 	gin_router.PUT("/items/:id", UpdateItemBuilder(gwg_api_label))
+	gin_router.DELETE("/items/:id", DeleteItemBuilder(gwg_api_label))
 	gin_router.POST("/items/:id/image", UploadItemImageBuilder(gwg_api_label))
 	gin_router.PUT("/restaurants/:id", UpdateRestaurantBuilder(gwg_api_label))
 	gin_router.GET("/restaurants/:id", GetRestaurantBuilder(gwg_api_label))
 	gin_router.DELETE("/restaurants/:id", DeleteRestaurantBuilder(gwg_api_label))
-	gin_router.POST("/restaurants/:id/tables", CreateTableBuilder(gwg_api_label))
-	gin_router.GET("/restaurants/:id/tables", ListRestaurantTableBuilder(gwg_api_label))
+	gin_router.POST("/restaurants", CreateRestaurantBuilder(gwg_api_label))
+	gin_router.GET("/restaurants", ListRestaurantsBuilder(gwg_api_label))
+	gin_router.POST("/restaurants/:id/items", CreateItemBuilder(gwg_api_label))
+	gin_router.GET("/restaurants/:id/items", ListRestaurantItemsBuilder(gwg_api_label))
 }
 func stringToInt32(s string) int32 {
 	if value, err := strconv.ParseInt(s, 10, 32); err == nil {
